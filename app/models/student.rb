d6e4abcf795ed464e0fe3c3552b964cc5d121fd2
Virginia Dooley <vcdooley@gmail.com>
2019-02-06 10:29:22 +0000

    Done.

---
+++ app/models/student.rb
@@ -0,0 +1,11 @@
+class Student < ActiveRecord::Base
+
+  def create!
+    @student = Student.create!(first_name: "Daenerys", last_name: "Targaryen")
+  end
+
+  def to_s
+    self.first_name + " " + self.last_name
+  end
+
+end
