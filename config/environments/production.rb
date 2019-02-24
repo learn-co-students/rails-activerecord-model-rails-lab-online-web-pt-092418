3395e18ca08dadb97ec7620e810723bce903052f
github username <github email address>
2019-02-23 20:31:21 -0500

    Done.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Do not dump schema after migrations.
+  # Do not dump schema after migrate.
   config.active_record.dump_schema_after_migration = false
 end
